0b6ff232d4eb97b1f9a402689fa1bfae577cd09d
Nishant <getrooted0019@hotmail.com>
2022-02-21 18:34:20 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="MacToManufacturer",
-    version="0.1.0",
+    version="0.2.0",
     author="Nishant Bhandari",
     author_email="getrooted0019@hotmail.com",
     description="Library to find manufacturer of a device using macaddress.",
